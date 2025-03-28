8bcae271c883ceeb034ab3307ddd4919d6986a94
vishwas1709 <vishwaschandramc2003@gmail.com>
2025-03-28 11:06:37 +0530

    for loop added

--- for_loop.py
+++ for_loop.py
@@ -10,3 +10,16 @@ for h in "Rama":
     
 for k in "Rama":
     print(k,end=" ")
+
+for a in range(100):
+    print(a, end=" ")
+print("")
+for b in range(1,100):
+    print(b,end=" ")
+print("")
+for c in range(1,100,2): #for odd numbers
+    print(c,end=" ")
+print("")
+for d in range (2,100,2): #for even numbers
+    print(d, end=" ")
+print("")
