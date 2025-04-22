c3e311a7248be3c43e8e98f774b96bf7c15969f3
vishwas1709 <vishwaschandramc2003@gmail.com>
2025-04-22 21:08:52 +0530

    22-04

---
+++ UpperCase_to_LowerCase.py
@@ -0,0 +1,16 @@
+print("Enter the string: ")
+str = input()
+i=0
+newstr = ""
+while (i<=len(str)-1):
+    data=str[i]
+    ascii = ord(data)
+    if(ascii >= 65 and ascii <= 90):
+        newascii = ascii +32
+        convchar = chr(newascii)
+        newstr = newstr + convchar
+    else:
+        newstr = newstr + data
+    i = i + 1
+print("The converted string is: ", newstr)
+# Compare this snippet from UpperCase_to_LowerCase.py:
---
+++ string_comparision.py
@@ -0,0 +1,10 @@
+print("Enter the first string:")
+str1 = input()
+print("Enter the second string:")
+str2 = input()
+if str1 > str2:
+    print("The first string is greater than the second string.")
+elif str1 > str2:
+    print("The second string is greater than the first string second string.")
+else:
+    print("The two strings are equal.")
