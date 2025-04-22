8677d81e420d72dd5c8d0c6fb311a978f5885f7a
vishwas1709 <vishwaschandramc2003@gmail.com>
2025-04-22 19:41:52 +0530

    19-04

---
+++ Arithmetic_On_String.py
@@ -0,0 +1,12 @@
+str1="rama"
+str2="sita"
+str3=str1+str2
+print(str3)  # Concatenation of str1 and str2
+# str4=str1-str2   Not possible
+# str5=str1*str2   Not possible
+# str6=str1/str2   Not possible
+# str7=str1%str2   Not possible
+# str8=str1**str2  Not possible
+str7=str1*2
+print(str7)  # Repetition of str1
+# str8=str1/2   Not possible
---
+++ String_Interning.py
@@ -0,0 +1,19 @@
+str1="rama"
+str2="sita"
+str3="ravana"
+str4="sita"
+str5="sita"
+str6="rama"
+str7="ravana"
+print(str1==str2)  # False
+print(str1)  # "rama"
+print(str2)  # "sita"
+print(str3)  # "ravana"
+print(str4)  # "sita"
+print(str5)  # "sita"
+print(str6)  # "rama"
+print(str7)  # "ravana"
+print(id(str1))  # Memory address of str1
+print(id(str2))  # Memory address of str2
+print(id(str6))  # Memory address of str6
+print(hex(id(str1)))  # Hexadecimal representation of memory address of str1
