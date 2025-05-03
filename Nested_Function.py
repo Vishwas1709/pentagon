8106fae237fd3f7b208173310b282f9f055f5586
vishwas1709 <vishwaschandramc2003@gmail.com>
2025-05-03 22:42:20 +0530

    02-05-25

---
+++ Nested_Function.py
@@ -0,0 +1,34 @@
+# def out_fun():
+#     print("Outer function")
+#     def in_fun():
+#         print("Inner function")
+#     in_fun()
+# out_fun()
+# print("End of program")
+
+# def fun1():
+#     a=10
+#     print("From fun1 a",a)
+#     def fun2():
+#         b=20
+#         print("From fun2 b",b)
+#         print("From fun2 a",a)
+#     fun2()
+#     print("From fun1 a",a)
+# fun1()
+# print("End of program")
+
+
+# def fun1():
+#     a=10
+#     print("From fun1 a",a)
+#     def fun2():
+#         a=100
+#         b=20
+#         print("From fun2 b",b)
+#         print("From fun2 a",a)
+#     fun2()
+#     print("From fun1 a",a)
+#     #print("From fun1 b",b)  # This will raise an error because b is not defined in this scope
+# fun1()
+
