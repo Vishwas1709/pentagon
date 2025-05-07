e5dc198c096a72be67d903f34340c5f523fe3028
vishwas1709 <vishwaschandramc2003@gmail.com>
2025-05-07 13:30:01 +0530

    07-05

--- Decorator.py
+++ Decorator.py
@@ -9,19 +9,41 @@
 
 
 #Control flow of Decorator functions
+#Defination 1
+# def print_msg():
+#     print("Hello EveryOne")
+# def Outer(print1):
+#     print("Entering Outer")
+#     def inner():
+#         print("Entering Inner")
+#         ref =print1
+#         ref()
+#         print("Leaving Inner")
+#     print("Calling Inner")
+#     return inner  # Return the inner function without calling it
+# ptr1=print_msg  # Assign the function to a variable
+# ptr2=Outer(ptr1)  # Call the outer function and get the reference to the inner function
+# ptr2()  # Call the inner function using the reference
+# print("Program Ended")
+
+#Definition 2
 
 def print_msg():
-    print("Hello EveryOne")
+    msg="Hello EveryOne"
+    return msg
 def Outer(print1):
     print("Entering Outer")
     def inner():
         print("Entering Inner")
         ref =print1
-        ref()
+        res=ref()
+        res1=res.upper()
+        print(res1)
         print("Leaving Inner")
-    print("Calling Inner")
     return inner  # Return the inner function without calling it
 ptr1=print_msg  # Assign the function to a variable
 ptr2=Outer(ptr1)  # Call the outer function and get the reference to the inner function
 ptr2()  # Call the inner function using the reference
 print("Program Ended")
+#In the above code, the outer function is called first, and it returns the inner function. The inner function is then called, which in turn calls the original function passed to it. This allows for additional functionality to be added before or after the original function is executed.
+
---
+++ Function_Overloading.py
@@ -0,0 +1,19 @@
+# def fun():
+#     print("Firsgt Function")
+# def fun(a):
+#     print("Second Function")
+# def fun(a,b):
+#     print("Third Function")
+# def fun(a,b,c):
+#     print("Fourth Function")
+# fun() # Give error as no argument is passed   fun() missing 3 required positional arguments: 'a', 'b', and 'c'
+
+def fun():
+    print("First Function")
+def fun():
+    print("Second Function")
+def fun():
+    print("Third Function")
+def fun():
+    print("Fourth Function")
+fun() # This will call the last defined function, which is "Fourth Function"
