c2f3fa9aec2cf07b0959234b17e21cf0ac0080a6
vishwas1709 <vishwaschandramc2003@gmail.com>
2025-06-05 13:44:36 +0530

    05-06

--- Dictionaries_in_python.py
+++ Dictionaries_in_python.py
@@ -4,3 +4,16 @@ print(type(emp_info))  # Output: <class 'dict'>
 print(emp_info["name"])  # Output: Rama
 emp_info["Mobile"]="1234567890"  # Adding a new key-value pair
 print(emp_info)  # Output: {'name': 'Rama', 'age': 30, 'height': 5.6, 'address': 'Mandya', 'Mobile': '1234567890'}
+print("===================================================")
+student_info = {"Name": "Rama","Age":22,"Height":5.6,"Address":"Mandya"}
+print(student_info)  # Output: {'Name': 'Rama', 'Age': 22, 'Height': 5.6, 'Address': 'Mandya'}
+print(student_info["Age"])  # Output: 22
+
+for i in student_info:
+    print(i)  # Prints the keys: Name, Age, Height, Address
+for i in student_info:
+    print(student_info[i])  # Prints the values: Rama, 22, 5.6, Mandya
+for i in student_info.values():
+    print(i)  # Prints the values: Rama, 22, 5.6, Mandya
+for index,values in student_info.items():
+    print(index,values)  # Prints key-value pairs: Name Rama, Age 22, Height 5.6, Address Mandya
